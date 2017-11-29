8d22bdcd0c9735507cc7cbecf3fe42c3a0795d1f
Nick <nspurlock@unm.edu>
2017-11-28 21:47:05 -0700

    Working in input still, need to verify correction behavior

--- basic.hs
+++ basic.hs
@@ -1,88 +1,114 @@
+import Parselib
 import System.IO
 import System.Environment
 import Control.Monad
 import Control.Monad.State
-import Control.Monad.Writer
+import Control.Monad.Reader hiding (ask)
 import Control.Monad.Trans
-import Control.Monad.Trans.Maybe
 import Control.Monad.Trans.State hiding (get, put)
-import Control.Monad.Trans.Writer hiding (tell)
-import Control.Monad.Trans.List
-import Control.Monad.IO.Class
---import Control.Monad.State.Class hiding (get, put)
-import Control.Monad.Writer.Class
+import Control.Monad.Trans.Reader
+import Control.Monad.IO.Class --liftIO
 import Data.Char
+import Data.Array.IO
 
-type Parser a = StateT String (MaybeT IO) a
+data Sexpr = Symbol String | Character Char | Number Int | Boolean Bool | Nil | Void | Cons {car :: Sexpr, cdr :: Sexpr} | Input (Char, String)
+--data Basic = INPUT (Char, String) | LET Sexpr Sexpr
+--type KeyValue = (Char, Sexpr)
 
---parse s = runStateT (runWriterT (input s)) ""
+instance Eq Sexpr where
+  Symbol x == Symbol y = x == y
+  Number x == Number y = x == y
+  Boolean x == Boolean y = x == y
+  Nil == Nil = True
 
-item :: Parser Char
-item = do
-  (x:xs) <- get
-  put xs
-  return x
+instance Show Sexpr where
+  show (Symbol x) = x
+  show (Number x) = show x
+  show (Boolean x) = show x
+  show (Character c) = show c
+  show Nil = "[]"
 
-char :: Char -> Parser Char
-char c = sat (c==)
+parse' :: String -> Sexpr
+parse' line = fst . head . (apply b) $ line
 
-sat :: (Char -> Bool) -> Parser Char
-sat p = do
-  c <- item
-  if p c then return c else mzero
+eval :: Sexpr -> ReaderT Environment IO Sexpr
+eval (Input (c, s)) = do
+  (table, _) <- ask
+  liftIO $ writeArray table c s
+  liftIO $ putStrLn $ "Added " ++ s ++ " to table"
+  return Void
 
-string :: String -> Parser String
-string [] = return ""
-string (c:cs) = do {char c; string cs; return (c:cs)}
+eval (Character c) = do
+  (table, _) <- ask
+  q <- liftIO $ readArray table c 
+  liftIO $ putStrLn q
+  s <- liftIO $ getLine
+  return (Symbol s)
 
-many :: Parser a -> Parser [a]
-many p = many1 p `mplus` mzero
+eval Void = do
+  return (Symbol "Failed")
 
-many1 :: Parser a -> Parser [a]
-many1 p = do {a <- p; as <- many p; return (a:as)}
+b :: Parser Sexpr
+b = input +++ var +++ (return Void)
 
-querry :: Parser (IO ())
-querry = do
-  s <- many (sat (/= '"'))
-  return (putStrLn s)
-
-spaces :: Parser String
-spaces = many (sat isSpace)
-
-token :: Parser String
-token = many (sat isAlphaNum)
-
-
---interpret :: Parser (IO ())
---interpret = input `mplus` mzero
-  
-
-input :: Parser [(String, IO String)]
+input :: Parser Sexpr
 input = do
   char '('
-  lnNum <- many (sat isDigit)
-  spaces
-  string "input"
-  spaces
-  q <- querry
-  spaces
-  key <- token
-  next <- input
-  return ((key, getLine):next)
+  token $ many (sat isDigit)
+  token $ string "input"
+  char '"'
+  q <- many alphanum
+  token $ char '"'
+  c <- token $ alphanum
+  char ')'
+  return (Input (c, q))
+
+var :: Parser Sexpr
+var = do
+  char '('
+  token $ many (sat isDigit)
+  c <- token $ (sat isAlpha)
+  char ')'  
+  return (Character c)
+
+--Should hold the symbol table and program <- use IOArray at some point
+type Environment = (IOArray Char String, IOArray Int String)
+
+basic :: Int -> ReaderT Environment IO ()
+basic n = do
+  liftIO . putStr $ "> "
+  --let s = (Number 5)
+  (sTable, prog) <- ask
+  --a <- liftIO $ readArray arr1 'a'
+  --liftIO $ writeArray arr1 'a' (Number 5)
+  --b <- liftIO $ readArray arr1 'a'
+  --liftIO $ putStrLn $ "Was" ++ (show a) ++ " now " ++ (show b)
+  --m <- readArray(arr1 'a')
+  --print m
+  --get line and put into IOArray
+  line <- liftIO $ getLine
+  if line == "quit" then return () else do
+    liftIO $ writeArray prog n line
+    result <- eval (parse' line)
+    case result of
+      Void -> basic (n + 1)
+      result' -> do
+        x <- eval result'
+        liftIO $ putStrLn (show x)
+        basic (n + 1)         
+  --liftIO $ writeArray prog n line
+  --liftIO $ writeArray sTable c s
+  --e <- liftIO $ readArray sTable c
+  
+   
 
 
---mainLoop :: Show a => String -> Maybe a
---mainLoop s = runStateT interpret s
-  
---main = do
---  args <- getArgs
---  case args of
---    [fName] -> do
---      handle <- openFile fName ReadMode
---      contents <- hGetContents handle
---      case (mainLoop contents) of
---        Just a -> do {a}
---        Nothing -> do {putStrLn "Failed"}
---      hClose handle
---    _ -> putStrLn "No input file specified!"
-  
+
+--main :: IO ()
+main = do
+  --[fName] <- getArgs
+  --handle <- openFile fName ReadMode
+  arr1 <- newArray ('a', 'z') "" :: IO (IOArray Char String)
+  arr2 <- newArray (1,200) "" :: IO (IOArray Int String)
+  let environment = (arr1, arr2)
+  runReaderT (basic 1) environment
